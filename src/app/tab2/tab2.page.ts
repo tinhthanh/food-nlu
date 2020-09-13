7cfe48c609446baf8a062db29f80f59fe1b3f3ac
alex.huynh <alex.huynh@pal.net.vn>
2020-09-13 15:19:11 +0700

    deploy

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService, Product} from '../services/cart.service';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
-  constructor() {}
-
+export class Tab2Page implements OnInit   {
+  cartItemCount: BehaviorSubject<number>;
+  cart: Product[] = [];
+  constructor(private cartService: CartService) {
+    this.cartItemCount = this.cartService.getCartItemCount();
+  }
+  ngOnInit() {
+    this.cart = this.cartService.getCart();
+  }
+  getTotal() {
+    return this.cart.reduce((i, j) => i + j.price * j.amount, 0);
+  }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { CartService } from '../services/cart.service';
+import {CartService, Product} from '../services/cart.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage  {
   cartItemCount: BehaviorSubject<number>;
+
   constructor(private cartService: CartService) {
     this.cartItemCount = this.cartService.getCartItemCount();
   }
+
+
+
 
 }
