ccee0b6d4533981d29ed2848b305ddf18b3014dc
phuongnhi123 <42488571+phuongnhi123@users.noreply.github.com>
2021-03-27 13:35:06 +0700

    show profit

--- src/app/pages/wefinex/wefinex.component.ts
+++ src/app/pages/wefinex/wefinex.component.ts
@@ -1,119 +1,129 @@
 import { AfterViewInit, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import { WefinexCommandService , WefinetComand} from 'src/app/services/wefinex-command.service';
+import { WefinexCommandService, WefinetComand } from 'src/app/services/wefinex-command.service';
 import { WefinexResultService, WefinexResult } from 'src/app/services/wefinex-result.service';
 import { interval, timer } from 'rxjs';
 import { takeUntil, timeInterval, timeout } from 'rxjs/operators';
 import AudioTouchUnlock from './audio-touch-unblock';
+import { Profit, WefinexTotalAmountService } from 'src/app/services/wefinex-total-amount.service';
 @Component({
   selector: 'app-wefinex',
   templateUrl: './wefinex.component.html',
   styleUrls: ['./wefinex.component.scss'],
 })
 export class WefinexComponent implements OnInit, AfterViewInit, OnDestroy {
-  mapType =  { G : {b : "#fee4cb" , t: "#ff942e", f: "Giảm"} , T : {b : "#c8f7dc", t: "#34c471" , f:"Tăng"}};
-  mapResult = {THUA: "Lose" , THANG :"WIN"};
+  mapType = { G: { b: '#fee4cb', t: '#ff942e', f: 'Giảm' }, T: { b: '#c8f7dc', t: '#34c471', f: 'Tăng' } };
+  mapResult = { THUA: 'Lose', THANG: 'WIN' };
   result: WefinexResult[];
   current: WefinetComand;
-  soundOn: boolean = window.localStorage.getItem("soundLive") != 'false';
+  soundOn: boolean = window.localStorage.getItem('soundLive') !== 'false';
+  profit = 0;
+  buget  = 0 ;
+  totalAmount = 0;
   constructor(@Inject(DOCUMENT) private document: Document, private wefinexCommandService: WefinexCommandService,
-   private wefinexResultService: WefinexResultService) {
-      this.wefinexResultService.getListByCondition( (ref) =>  ref.orderBy("lastUpdate", "desc").limit(8)).subscribe((k) => {
-         this.result  = k;
-      });
-  
-      this.wefinexCommandService.get('command').subscribe( (data) => {
-        const d = new Date();
-        const hours = String(d.getHours()).padStart(2, '0') ;
-        const minute = String(d.getMinutes()).padStart(2, '0')   ;
-        
-        const day =  String(d.getDate()).padStart(2, '0') ;
-        const month =   String(d.getMonth() + 1).padStart(2, '0') ;
-        const year = d.getFullYear();
-        
-        const datePlace = data.time.split(' ')[0] ;
-        const timePlace = data.time.split(' ')[1] ;
-        const currentTime =  `${day}:${month}:${year} ${hours}:${minute}` ;
-        console.log(currentTime) ;
-        console.log(data.time)
-        if(currentTime === data.time) { 
-              this.current = data;
-              this.playSound(`assets/wefinex/sounds/${this.current.type}.mp3`);
-             const time =  timer((60 - new Date().getSeconds())*1000);
-             time.subscribe(( d ) => {
-              this.current = undefined;
-            });
-              interval(500).pipe(takeUntil(time))
-              .subscribe(
-                  value => {
-                    this.current['countDown'] = Math.max(60 - new Date().getSeconds() , 0) ;
-                    this.current['percent'] = Number(parseFloat(( ((60 - this.current['countDown'] )/60)*100)+"").toFixed(2));
-                  } ,
-                  err =>   this.current = undefined
-              );
-        } else {
-          this.playSound(`assets/wefinex/sounds/warning.mp3`);
+              private wefinexResultService: WefinexResultService, private wefinexTotalAmountService : WefinexTotalAmountService) {
+    this.wefinexResultService.getListByCondition((ref) => ref.orderBy('lastUpdate', 'desc').limit(8)).subscribe((k) => {
+      this.result = k;
+    });
+    const d = new Date();
+    const hours = String(d.getHours()).padStart(2, '0');
+    const minute = String(d.getMinutes()).padStart(2, '0');
+
+    const day = String(d.getDate()).padStart(2, '0');
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const year = d.getFullYear();
+    this.wefinexTotalAmountService.get(`${day}:${month}:${year}`).subscribe( (data) => {
+        console.log(data);
+        this.buget = Number(data.budget);
+        this.totalAmount = Number(data.totalAmount);
+        this.profit = (Number(data.profit) / Number(data.budget)) * 100;
+
+    });
+    this.wefinexCommandService.get('command').subscribe((data) => {
+
+      const datePlace = data.time.split(' ')[0];
+      const timePlace = data.time.split(' ')[1];
+      const currentTime = `${day}:${month}:${year} ${hours}:${minute}`;
+      console.log(currentTime);
+      console.log(data.time)
+      if (currentTime === data.time) {
+        this.current = data;
+        this.playSound(`assets/wefinex/sounds/${this.current.type}.mp3`);
+        const time = timer((60 - new Date().getSeconds()) * 1000);
+        time.subscribe((d) => {
           this.current = undefined;
-        }
-    
-      })
+        });
+        interval(500).pipe(takeUntil(time))
+          .subscribe(
+            value => {
+              this.current['countDown'] = Math.max(60 - new Date().getSeconds(), 0);
+              this.current['percent'] = Number(parseFloat((((60 - this.current['countDown']) / 60) * 100) + '').toFixed(2));
+            },
+            err => this.current = undefined
+          );
+      } else {
+        this.playSound(`assets/wefinex/sounds/warning.mp3`);
+        this.current = undefined;
+      }
+
+    })
   }
   initSound() {
-    if(!this.soundOn)  return;
+    if (!this.soundOn) return;
     const audio = new Audio(`assets/wefinex/sounds/warning.mp3`);
     audio.muted = true;
-    const alert_elem = document.querySelector( '#trigger-volume' );
-    audio.play().then( () => {
+    const alert_elem = document.querySelector('#trigger-volume');
+    audio.play().then(() => {
       this.soundOn = true;
-      window.localStorage["soundLive"] = this.soundOn;
-    } )
-    .catch( () => {
-      this.soundOn = false;
-      window.localStorage["soundLive"] = this.soundOn;
-      alert_elem.addEventListener( 'click', () => {
-            audio.play()
-              .then();
-              this.soundOn = true;
-              window.localStorage["soundLive"] = this.soundOn;
-      } );
-    } );
+      window.localStorage['soundLive'] = this.soundOn;
+    })
+      .catch(() => {
+        this.soundOn = false;
+        window.localStorage['soundLive'] = this.soundOn;
+        alert_elem.addEventListener('click', () => {
+          audio.play()
+            .then();
+          this.soundOn = true;
+          window.localStorage['soundLive'] = this.soundOn;
+        });
+      });
   }
   clickIconVolume(): void {
     this.soundOn = !this.soundOn;
-    window.localStorage["soundLive"] = this.soundOn;
-   
+    window.localStorage['soundLive'] = this.soundOn;
+
   }
   ngOnDestroy(): void {
     AudioTouchUnlock.onDestroy();
   }
   ngAfterViewInit(): void {
     this.initSound();
-      const modeSwitch = this.document.querySelector('.mode-switch');
-      modeSwitch.addEventListener('click', () => {
-        this.document.documentElement.classList.toggle('dark');
-        modeSwitch.classList.toggle('active');
-      });
-      const listView = this.document.querySelector('.list-view');
-      const gridView = this.document.querySelector('.grid-view');
-      const projectsList = this.document.querySelector('.project-boxes');
-      listView.addEventListener('click', () => {
-        gridView.classList.remove('active');
-        listView.classList.add('active');
-        projectsList.classList.remove('jsGridView');
-        projectsList.classList.add('jsListView');
-      });
-      gridView.addEventListener('click', () => {
-        gridView.classList.add('active');
-        listView.classList.remove('active');
-        projectsList.classList.remove('jsListView');
-        projectsList.classList.add('jsGridView');
-      });
-      this.document.querySelector('.messages-btn').addEventListener('click', () => {
-        this.document.querySelector('.messages-section').classList.add('show');
-      });
-      this.document.querySelector('.messages-close').addEventListener('click', ()  => {
-        this.document.querySelector('.messages-section').classList.remove('show');
-      });
+    const modeSwitch = this.document.querySelector('.mode-switch');
+    modeSwitch.addEventListener('click', () => {
+      this.document.documentElement.classList.toggle('dark');
+      modeSwitch.classList.toggle('active');
+    });
+    const listView = this.document.querySelector('.list-view');
+    const gridView = this.document.querySelector('.grid-view');
+    const projectsList = this.document.querySelector('.project-boxes');
+    listView.addEventListener('click', () => {
+      gridView.classList.remove('active');
+      listView.classList.add('active');
+      projectsList.classList.remove('jsGridView');
+      projectsList.classList.add('jsListView');
+    });
+    gridView.addEventListener('click', () => {
+      gridView.classList.add('active');
+      listView.classList.remove('active');
+      projectsList.classList.remove('jsListView');
+      projectsList.classList.add('jsGridView');
+    });
+    this.document.querySelector('.messages-btn').addEventListener('click', () => {
+      this.document.querySelector('.messages-section').classList.add('show');
+    });
+    this.document.querySelector('.messages-close').addEventListener('click', () => {
+      this.document.querySelector('.messages-section').classList.remove('show');
+    });
   }
   trackByFn(index, item) {
     return item.id;
--- src/app/services/wefinex-total-amount.service.ts
+++ src/app/services/wefinex-total-amount.service.ts
@@ -2,47 +2,41 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Entity, FirestoreCrudService } from './firestore-crud.service';
-
 @Injectable({
   providedIn: 'root'
 })
-export class WefinexResultService {
-
-
-  private crudService: FirestoreCrudService<WefinexResult>;
-
+export class WefinexTotalAmountService {
+  private crudService: FirestoreCrudService<Profit>;
   // AngularFirestore should be found by Angular DI System
   constructor(private afs: AngularFirestore) {
       // Let's create our CrusService and use the a Collection with the name 'todos'
-      this.crudService = new FirestoreCrudService<WefinexResult>(afs, 'wefinex');
+        this.crudService = new FirestoreCrudService<Profit>(afs, 'wefinex_total_amount');
   }
-
-  add(data: WefinexResult, id?: string) {
+  add(data: Profit, id?: string) {
       return this.crudService.add(data, id);
   }
-
-  update(data: WefinexResult) {
+  update(data: Profit) {
       return this.crudService.update(data);
   }
-
-  delete(data: WefinexResult) {
+  delete(data: Profit) {
       return this.crudService.delete(data.id);
   }
-
   getAll() {
       return this.crudService.list();
   }
-  getListByCondition(query: (ref: any) => any): Observable<WefinexResult[]> {
+  getListByCondition(query: (ref: any) => any): Observable<Profit[]> {
     return this.crudService.listByCondition(query);
    }
+   get(id): Observable<Profit> {
+    return this.crudService.get(id);
+  }
 }
 
 
-export class WefinexResult implements Entity {
-  action: string; 
-  price: string;
+export class Profit implements Entity {
+  budget: string;
+  profit: string;
   lastUpdate: Date;
   id?: string;
-  time: string;
-  type: string;
+  totalAmount: string;
 }
